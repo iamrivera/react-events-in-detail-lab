611e25f277fbf7ceb777e07a189c9e9c8900a02d
iamrivera <a3m3rivera@gmail.com>
2021-09-12 12:47:05 -0700

    commit tree 2ca6fb02cf8fcd8517c8f0c0ca463d805d604aa4

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,21 @@
 // Code CoordinatesButton Component Here
+
+import React, {Component } from 'react'; 
+
+export default class CoordinatesButton extends Component {
+    
+    handleClick = (event) => {
+        let coordX = event.clientX
+        let coordY = event.clientY
+
+        let coordArray = [coordX,coordY]
+
+        this.setState({
+            coordinates: this.props.onReceiveCoordinates(coordArray)
+        })
+    }
+
+    render(){
+        return <button onClick={this.handleClick}> Get Coordinates </button>
+    }
+}
